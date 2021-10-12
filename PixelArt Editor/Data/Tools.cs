6f18b0022317c57bfe9f61eaf642fce602119a47
Vojtěch Kursa <it.vojtechkursa@gmail.com>
2021-10-12 16:24:55 +0200

    Create Toolbar Module

---
+++ PixelArt Editor/Data/Tools.cs
@@ -0,0 +1,4 @@
+﻿namespace PixelArt_Editor.Data
+{
+    public enum Tools { Brush, Line, Rectangle }
+}
---
+++ PixelArt Editor/GUI/Modules/ToolbarModule.xaml.cs
@@ -0,0 +1,24 @@
+﻿using PixelArt_Editor.Data;
+using System.Windows.Controls;
+using Color = System.Drawing.Color;
+
+namespace PixelArt_Editor.GUI.Modules
+{
+    /// <summary>
+    /// Interaction logic for ToolbarModule.xaml
+    /// </summary>
+    public partial class ToolbarModule : UserControl
+    {
+        public Color Color
+        {
+            get { return CSM_colors.Color; }
+        }
+
+        public Tools Tool { get; }
+
+        public ToolbarModule()
+        {
+            InitializeComponent();
+        }
+    }
+}
